fee0cf72f109339d67fc5184e62182f2efa25e72
AmerDidit <amer@amerdid.it>
2014-04-27 15:55:27 +0200

    add maximum program.

---
+++ programs/Maximum.pas
@@ -0,0 +1,19 @@
+program Maximum (input, output);
+{
+  Gesucht ist ein Programm Maximum, welches das Maximum einer Eingabefolge von integer-Zahlen bestimmt und ausgibt.
+  Im Unterschied zu Beispiel 4.1.1.3 im Kurstext ist hier die Anzahl der Folgenelemente nicht als Konstante vorgegeben,
+  sondern die Eingabefolge wird mit der Zahl 0, die nicht mehr zur Folge gehört, abgeschlossen.
+  Hinweis: Beachten Sie den Fall einer leeren Eingabefolge,
+  d.h. den Fall, daß lediglich eine Null eingegeben wird! In diesem Fall soll folgende Anweisung
+
+  writeln('Leere Eingabefolge!')
+  ausgeführt werden.
+}
+
+var
+var1,
+var2 : integer;
+
+begin
+
+end.
